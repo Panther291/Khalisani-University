65182acb9824b2e7a9dc08689f9909f113d7be19
Panther-ANK <thepanther291@gmail.com>
2024-08-26 22:08:15 +0530

    z

--- assets/js/scrollanimation.js
+++ assets/js/scrollanimation.js
@@ -10,22 +10,7 @@ const obs = new IntersectionObserver((entries) => {
       }
     })
   })
-  
-//   const obs2 = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//       console.log(entry);
-//       if(entry.isIntersecting){
-//         entry.target.classList.add('show-el')
-//       }
-      
-//       else{
-//         entry.target.classList.remove('show-el')
-//       }
-//     })
-//   })
-  
+
   const hidden = document.querySelectorAll('.hidden-sec')
-//   const hidden2 = document.querySelectorAll('.hidden-el')
   hidden.forEach((e) => obs.observe(e))
-//   hidden2.forEach((e) => obs.observe(e))
   
